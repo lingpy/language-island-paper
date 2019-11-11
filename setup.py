7ff1858ffb74b8a4fcd1357cc575f55027d9bb3f
chrzyki <christoph@functor-argument.net>
2019-11-11 15:25:07 +0100

    Cleaned up inconsistencies, updated requirements, re-generated CLDF with up-to-date FormSpec.

--- setup.py
+++ setup.py
@@ -2,28 +2,21 @@ from setuptools import setup, find_packages
 import json
 
 
-with open('metadata.json') as fp:
+with open("metadata.json") as fp:
     metadata = json.load(fp)
 
 
 setup(
-    name='lexibank_hantganbangime',
+    name="lexibank_hantganbangime",
     version="1.2.0",
-    description=metadata['title'],
-    license=metadata.get('license', ''),
-    url=metadata.get('url', ''),
-    py_modules=['lexibank_hantganbangime'],
-    packages=find_packages(where='src'),
-    package_dir={'': 'src'},
+    description=metadata["title"],
+    license=metadata.get("license", ""),
+    url=metadata.get("url", ""),
+    py_modules=["lexibank_hantganbangime"],
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
     include_package_data=True,
     zip_safe=False,
-    entry_points={
-        'lexibank.dataset': [
-            'hantganbangime=lexibank_hantganbangime:Dataset',
-        ]
-    },
-    install_requires=[
-        'pylexibank>=2.0.0',
-    ]
+    entry_points={"lexibank.dataset": ["hantganbangime=lexibank_hantganbangime:Dataset"]},
+    install_requires=["pylexibank>=2.1"],
 )
-
